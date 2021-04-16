c590b9b9f8830bb17774ede47f7f3ca2a29e5b29
Mark Abdilla <markabdilla26@gmail.com>
2021-04-16 11:41:52 +0200

    Grid changes to black or white on mouse hover

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ for (i = 1; i <= gridCount; i++) {
     document.addEventListener('DOMContentLoaded', function(){
         let div = document.createElement('div');
         div.className = 'grid-box';
+        div.addEventListener('mouseover', changeColour);
+        div.addEventListener('mouseout', revertColour);
         gridContainer.appendChild(div);
     });
 }
+
+function changeColour(e)
+{
+    e.target.style.backgroundColor = "black";
+}
+
+function revertColour(e)
+{
+    e.target.style.backgroundColor = "white";
+}
+
