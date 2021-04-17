2ef724882894f1657bb7d46537eaabb34a22c580
Mark Abdilla <markabdilla26@gmail.com>
2021-04-17 14:03:25 +0200

    Creation of reset button and code refactor

--- app.js
+++ app.js
@@ -1,15 +1,54 @@
-let gridContainer = document.querySelector('.container');
-let gridCount = 16;
+const gridContainer = document.querySelector('.container');
+const gridButton = document.getElementById('btn');
 
-for (i = 1; i <= gridCount; i++) {
-    document.addEventListener('DOMContentLoaded', function(){
-        let div = document.createElement('div');
+window.addEventListener('load', DefaultGrid);
+
+gridButton.addEventListener('click', changeGridSize);
+
+function DefaultGrid()
+{
+    setGridSize(16);
+    fillGrid(16);
+}
+
+function setGridSize(size) {
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+}
+
+function fillGrid(size) {
+    for (i = 0; i < size * size; i++) {
+        const div = document.createElement('div');
         div.className = 'grid-box';
         div.addEventListener('mouseover', changeColour);
         div.addEventListener('mouseout', revertColour);
         gridContainer.appendChild(div);
-    });
+    }
 }
+
+function changeGridSize()
+{
+    let newSize = prompt("Choose the new size of your grid: ", "Please enter a number");
+    if (newSize !== null)
+    {
+        newSize = parseInt(newSize);
+        if (newSize < 1 || newSize > 64 || Number.isNaN(newSize)) {
+            alert("Enter a number from 1 - 64 range");
+            changeGridSize();
+        }
+        else {
+            clearGrid();
+            setGridSize(newSize);
+            fillGrid(newSize);
+        }
+    }
+}
+
+function clearGrid() {
+    const gridArray = Array.from(gridContainer.childNodes);
+    gridArray.forEach((element) => {
+      gridContainer.removeChild(element);
+    });
+  }
 
 function changeColour(e)
 {
